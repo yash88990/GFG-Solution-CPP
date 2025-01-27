2d94f8533ea73985be68dcbcf840634270634a61
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-01-27 19:15:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two sum -Pairs with 0 Sum/two-sum-pairs-with-0-sum.cpp
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+class Matrix {
+  public:
+    template <class T>
+    static void input(vector<vector<T>> &A, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%d ", &A[i][j]);
+            }
+        }
+    }
+
+    template <class T>
+    static void print(vector<vector<T>> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < A[i].size(); j++) {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> getPairs(vector<int>& arr) {
+        // code here
+         unordered_set<int> seen;  // to store the elements we have visited
+    set<vector<int>> result;   // to store unique pairs
+
+    // Traverse the array
+    for (int num : arr) {
+        // Check if the negation of num exists in seen
+        if (seen.find(-num) != seen.end()) {
+            // Insert the pair in sorted order, smaller number first
+            result.insert({min(num, -num), max(num, -num)});
+        }
+        // Add the current number to the seen set
+        seen.insert(num);
+    }
+
+    // Convert the set of pairs to a vector of vectors
+    vector<vector<int>> resultVec(result.begin(), result.end());
+
+    return resultVec;
+    }
+};
+
+//{ Driver Code Starts.
+//Back-end complete function Template for C++
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        // char c;cin>>c;
+        Solution obj;
+        vector<vector<int>> res = obj.getPairs(arr);
+        if (res.size() == 0) {
+            cout << endl;
+        } else
+            Matrix::print(res);
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
