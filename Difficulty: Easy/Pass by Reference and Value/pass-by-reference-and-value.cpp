cdc27cf654b24fe0b265169d1e556d90d3b7b72b
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-02-02 02:22:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Pass by Reference and Value/pass-by-reference-and-value.cpp
+++ Difficulty: Easy/Pass by Reference and Value/pass-by-reference-and-value.cpp
@@ -12,9 +12,10 @@ class Solution {
   public:
     vector<int> passedBy(int a, int &b) {
         // code here
-        a = a + 1; 
-        b = b + 2; 
-        return {a,b};
+        a = a + 1 ;
+        b = b + 2 ;
+        return { a , b};
+       
     }
 };
 
