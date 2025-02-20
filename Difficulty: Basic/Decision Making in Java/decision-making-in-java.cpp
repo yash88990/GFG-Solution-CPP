cbd9285b461e8115b9a716d1d44f6b3ea1db992c
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-02-20 22:10:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Decision Making in Java/decision-making-in-java.cpp
+++ Difficulty: Basic/Decision Making in Java/decision-making-in-java.cpp
@@ -10,9 +10,10 @@ class Solution {
   public:
     string compareNM(int n, int m){
         // code here
-        if ( n == m ) return "equal";
-        else if ( n < m ) return "lesser";
-        else return "greater";
+        if(n < m )return "lesser";
+        else if(n == m )return "equal";
+        else if(n > m )return "greater";
+        else return "nothing";
     }
 };
 
