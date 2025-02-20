0762ea6b17c3c9c5a8dccfe719dcea7d6e6b310f
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-02-20 22:05:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Data Type/data-type.cpp
+++ Difficulty: Basic/Data Type/data-type.cpp
@@ -16,11 +16,12 @@ class Solution {
     int dataTypeSize(string str) {
         // your code here
         if(str == "Character")return sizeof(char);
-        else if (str == "Integer") return sizeof(int);
-        else if(str == "Long") return sizeof(long long);
-        else if(str == "Float") return sizeof(float);
-        else if (str == "Double") return sizeof(double);
+        else if(str == "Integer")return sizeof(int);
+        else if(str == "Long")return sizeof(long long);
+        else if(str == "Float")return sizeof(float);
+        else if(str == "Double")return sizeof(double);
         else return -1;
+        
     }
 };
 
