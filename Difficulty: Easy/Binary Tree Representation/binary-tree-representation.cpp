efce00956894fa1e4aad638449a8b3e6e89d2b93
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-02-24 16:46:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary Tree Representation/binary-tree-representation.cpp
+++ Difficulty: Easy/Binary Tree Representation/binary-tree-representation.cpp
@@ -56,24 +56,21 @@ public:
     void create_tree(node* root0, vector<int> &vec){
         //Your code goes here
         if (vec.empty() || !root0) return;
-    queue<node*> q;
-    q.push(root0);
-    int i = 1;
-
-    while (!q.empty() && i < vec.size()) {
-        node* current = q.front();
-        q.pop();
-
+        queue<node*> q;
+        q.push(root0);
+        int i = 1;
+        while (!q.empty() && i < vec.size()) {
+            node* current = q.front();
+            q.pop();
         // Create left child
-        current->left = newNode(vec[i++]);
-        q.push(current->left);
-
+            current->left = newNode(vec[i++]);
+             q.push(current->left);
         // Create right child (if elements remain)
-        if (i < vec.size()) {
-            current->right = newNode(vec[i++]);
-            q.push(current->right);
+            if (i < vec.size()) {
+                current->right = newNode(vec[i++]);
+                q.push(current->right);
+            }
         }
-    }
     }
 
 };
