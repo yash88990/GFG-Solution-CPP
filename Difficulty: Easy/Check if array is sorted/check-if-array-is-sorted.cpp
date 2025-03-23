ffd46d627f9f02becde6ca10af803597c8837b8e
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-03-23 21:39:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    bool arraySortedOrNot(vector<int>& arr) {
+        // code here
+        if(arr.size() == 0 || arr.size() == 1)return true;
+        for(int i = 1 ; i < arr.size() ; i++){
+            if(arr[i] < arr[i-1])return false;
+        }
+        return true;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        bool ans = ob.arraySortedOrNot(nums);
+        cout << (ans ? "true" : "false") << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
