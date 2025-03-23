921451718c6d7393321f6d6f53b95ba14627f82d
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-03-23 22:13:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move all negative elements to end/move-all-negative-elements-to-end.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void segregateElements(vector<int>& arr) {
+        // Your code goes here
+        vector<int> positive , negative;
+        for(int num : arr){
+            if(num >= 0 )  positive.push_back(num);
+            else negative.push_back(num);
+        }
+        int index = 0 ;
+        for(int num : positive ) arr[index++] = num;
+        
+        for(int num : negative) arr[index++] = num;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        ob.segregateElements(nums);
+
+        for (int x : nums)
+            cout << x << " ";
+        cout << "\n~\n";
+    }
+}
+// } Driver Code Ends
