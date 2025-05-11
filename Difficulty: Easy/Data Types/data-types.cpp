d5376576314d22863a60f6c47fcbd91bb060d534
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-11 20:27:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Data Types/data-types.cpp
@@ -0,0 +1,26 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+int main() {
+    // declare the variables here
+    int a;
+    float b;
+    double c;
+    long long d;
+
+    // variables declaration checking
+    if ((typeid(a) == typeid(int)) and (typeid(b) == typeid(float)) and
+        (typeid(c) == typeid(double)) and (typeid(d) == typeid(long long)))
+        cout << "verified\n";
+
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
