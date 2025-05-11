22dead5b75d1bb5d0ee119a3dbe09b26dc0b6bc6
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-11 20:42:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Type Conversion/type-conversion.cpp
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double d;
+    cin >> d;
+
+    // Typecast and Print
+    int ans = (int)d;
+    cout << ans << endl;
+
+    return 0;
+}
+
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
