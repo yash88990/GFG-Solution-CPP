6bcd28245ae51da5031fe140e0315bde0757c75f
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-17 08:20:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/For Loop- primeCheck/for-loop-prime-check.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    string isPrime(int n) {
+        if (n <= 1) return "No";
+
+        for (int i = 2; i <= sqrt(n); i++) {
+            if (n % i == 0) return "No";
+        }
+
+        return "Yes";
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int n;
+        cin >> n;
+
+        Solution obj;
+        string res = obj.isPrime(n);
+
+        cout << res << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+}
+
+// } Driver Code Ends
