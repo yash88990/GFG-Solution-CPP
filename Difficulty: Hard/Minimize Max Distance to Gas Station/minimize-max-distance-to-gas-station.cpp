73cfda57ace802c6bb104fd0abd54a69eee05baa
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-26 09:27:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
+++ Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
@@ -1,29 +1,25 @@
 class Solution {
   public:
-    int requiredStations(const vector<int>& stations, double dist) {
-        int count = 0;
-        for (int i = 1; i < stations.size(); i++) {
-            double gap = stations[i] - stations[i - 1];
-            count += int(gap / dist);
-        }
-        return count;
-    }
-
+  int requiredstations( const vector<int> &stations, double dist){
+      int count = 0;
+      for(int i = 1 ; i < stations.size() ; i++){
+          double gap = stations[i] - stations[i-1];
+          count += int(gap / dist);
+      }
+      return count;
+  }
     double findSmallestMaxDist(vector<int> &stations, int k) {
+        // Code here
         int n = stations.size();
-        double low = 0;
-        double high = stations[n - 1] - stations[0];
-        double eps = 1e-6;
-
-        while (high - low > eps) {
+        double low = 0 , high = stations[n - 1] - stations[0];
+        double eps= 1e-6;
+        
+        while(high - low > eps){
             double mid = (low + high) / 2.0;
-            int needed = requiredStations(stations, mid);
-            if (needed > k)
-                low = mid;
-            else
-                high = mid;
+            int needed = requiredstations(stations , mid);
+            if(needed > k)low = mid ;
+            else high = mid;
         }
-
-        return round(high * 100) / 100.0;  // Round to 2 decimal places
+        return round(high * 100) / 100.0;
     }
 };
