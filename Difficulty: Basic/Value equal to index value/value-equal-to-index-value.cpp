d26277ee6be2383532833d041ad72c169200723c
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-26 09:04:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Value equal to index value/value-equal-to-index-value.cpp
+++ Difficulty: Basic/Value equal to index value/value-equal-to-index-value.cpp
@@ -1,16 +1,24 @@
 // User function template for C++
 
 class Solution {
+    private:
+    void solve(vector<int> &arr , int start , int end , vector<int>&ans){
+        if(start > end)return ;
+        int mid = start + ( end - start) / 2 ;
+        if(arr[mid] == mid + 1)ans.push_back(mid + 1);
+        solve(arr , start , mid -1 , ans);
+        solve(arr , mid + 1 , end , ans);
+    }
   public:
     // Function to find elements in the array that are equal to their index.
     vector<int> valueEqualToIndex(vector<int>& arr) {
         // code here
-        int n = arr.size();
-        vector<int>ans;
-        for(int i = 0 ; i < n ; i++){
-            if(arr[i] == i+1)ans.push_back(i + 1);
-        }
-        return ans;
+       vector<int>ans;
+       int n = arr.size();
+       
+       solve(arr , 0 , n - 1 , ans);
+       sort(ans.begin() , ans.end());
+       return ans;
         
     }
 };
