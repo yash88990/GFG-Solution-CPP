8cf98a6fb9bd60621c04c251ec57aca8b1fae280
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-27 12:06:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Non Repeating Character/non-repeating-character.cpp
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.cpp
@@ -3,13 +3,12 @@ class Solution {
   public:
     char nonRepeatingChar(string &s) {
         // Your code here
-        unordered_map<char,int>freq;
-        for(char ch : s){
-            freq[ch]++;
-        }
+        unordered_map<char , int>freq;
+        for(char ch : s)freq[ch]++;
         for(char ch : s){
             if(freq[ch] == 1)return ch;
         }
         return '$';
+        
     }
 };
