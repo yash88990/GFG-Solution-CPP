a74970cf675bd692cfe5af523dcff3490fa12072
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-27 16:23:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print leaf nodes from preorder traversal of BST/print-leaf-nodes-from-preorder-traversal-of-bst.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    vector<int> leafNodes(vector<int>& preorder) {
+        int n = preorder.size();
+        vector<int> res;
+        stack<int> st;
+        st.push(preorder[0]);
+        for(int i = 1; i < n; i++){
+            // Take the current node
+            int currentNode = preorder[i];
+           if(currentNode > st.top()){
+                // So for figuring out, we can check how many nodes are smaller than current node
+                // If only 1 node is smaller it means the node on the top of the stack 
+                // is parent to current node and current node is right child of its
+                // But if smallerNodes are greater than 1, it means top node was the leaf node
+                int smallerNodes = 0; 
+                int leftMostNode = st.top();
+                while(!st.empty() && currentNode > st.top()){
+                    smallerNodes++;
+                    st.pop();
+                }
+                // If the count of smaller nodes are greater than 1
+                // we push it into res, which will store leaf nodes
+                if(smallerNodes > 1){
+                    res.push_back(leftMostNode);
+                }
+            }
+            // Whether the node is greater than st.top() or not, we push it in the stack
+            st.push(currentNode);
+        }
+        
+        // We push the last node in the array, as that will be definitely the rightmost leaf node in the tree
+        if(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        return res;
+    } 
+};
